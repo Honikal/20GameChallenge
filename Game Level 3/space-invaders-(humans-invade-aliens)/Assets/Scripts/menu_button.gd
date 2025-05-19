c16508a5eb583f0db155c61bae6fa1dcf054c278
Honikal <81885736+Honikal@users.noreply.github.com>
2025-05-19 17:31:27 -0600

    Agregamos modificaciones al menú

--- Game Level 3/space-invaders-(humans-invade-aliens)/Assets/Scripts/menu_button.gd
+++ Game Level 3/space-invaders-(humans-invade-aliens)/Assets/Scripts/menu_button.gd
@@ -6,14 +6,11 @@ extends TextureButton
 		if is_inside_tree():
 			_setupText();
 		
-@export var arrowMargin: int = 70;
 @export var waveSpeed: float = 5.0;
 @export var waveAmplitude: float = 30.0;
-@export var rainbowSpeed:  float = 2;
 
+@export var rainbowSpeed:  float = 2;
 @onready var label: RichTextLabel = $RichTextLabel
-@onready var left: Sprite2D = $Left
-@onready var right: Sprite2D = $Right
 
 #Este script se encarga de establecer botones con los que se puede interactuar por teclado
 func _ready() -> void:
@@ -23,11 +20,10 @@ func _ready() -> void:
 		
 	#Seteamos el botón para que funcione de forma interactiva
 	_setupText();
-	_hideArrows();
 	
 	#Manejamos las señales de los botones, cuando haya focus, activamos el caso que las flechas se activan
-	focus_entered.connect(_showArrows);
-	focus_exited.connect(_hideArrows);
+	focus_entered.connect(_setupText);
+	focus_exited.connect(_setupText);
 	#mouse_entered.connect(grab_focus);
 	
 func _setupText():
@@ -39,31 +35,17 @@ func _setupText():
 		#Aplicamos el efecto normal
 		label.text = "[center]%s[/center]" % text;
 	
-func _showArrows():
-	#Hacemos que las flechas sean visibles y actualizamos la posición de la flecha
-	left.visible = true;
-	right.visible = true;
-	_updateArrowPosition();
-	_setupText(); #Aplicamos efecto por si acaso
-
-func _hideArrows():
-	left.visible = false;
-	right.visible = false;	
-	_setupText(); #Aplicamos efecto para quitar por si acaso
-	
-func _updateArrowPosition():
-	#Calculamos las posiciones (x,y) del botón
-	var y_pos = global_position.y + (size.y / 3);
-	var x_pos = global_position.x + (size.x / 2);
-		
-	#Reubicamos las flechas
-	left.global_position  = Vector2(x_pos - arrowMargin, y_pos);
-	right.global_position = Vector2(x_pos + arrowMargin, y_pos);
-
 func _get_animated_text() -> String:
+	#Acá aplicaríamos diseño con rainbow, pero no aplicaremos wave
+	"""
 	return "[center][wave amp=%s freq=%s][rainbow freq=%s]%s[/rainbow][/wave][/center]" % [
 		waveAmplitude,
 		waveSpeed,
+		rainbowSpeed,
+		text
+	];
+	"""
+	return "[center][rainbow freq=%s]%s[/rainbow][/center]" % [
 		rainbowSpeed,
 		text
 	];
--- Game Level 3/space-invaders-(humans-invade-aliens)/Assets/Scripts/menu_scene.gd
+++ Game Level 3/space-invaders-(humans-invade-aliens)/Assets/Scripts/menu_scene.gd
@@ -4,10 +4,25 @@ extends Node
 	$MenuUI/ButtonsSection/PlayBtn,
 	$MenuUI/ButtonsSection/OptionsBtn
 ]	
-		
+@onready var optButtons = [
+	$MenuUI/OptionsSection/ResolutionSection,
+	$MenuUI/OptionsSection/FullscreenCheck,
+	$MenuUI/OptionsSection/VSyncCheck,
+	$MenuUI/OptionsSection/BackBtn
+]
+
+@onready var buttons_section: VBoxContainer = $MenuUI/ButtonsSection
+@onready var options_section: VBoxContainer = $MenuUI/OptionsSection
+@onready var res_option: OptionButton = $MenuUI/OptionsSection/ResolutionContainer/ResOption
+
 func _ready() -> void:
 	#La navegación entre vecinos está manejada por el editor, aunque se podría utilizar esta función
 	_setup_focus();
+	_setup_options();
+	
+	#Apagamos visualmente la sección de opciones y seteamos los valores iniciales
+	options_section.visible = false;
+	
 	
 	#Agarramos el focus del botón, primero empezamos con el de play, y luego intentamos implementar
 	#el manejo de keyboard desde el mismo botón
@@ -20,22 +35,37 @@ func _connect_signals():
 	#Conectamos las señales de los botones usando un método lambda
 	for btn in buttons:
 		btn.pressed.connect(_on_button_pressed.bind(btn));
+		
+	#Conectamos el botón de salida de opciones
+	optButtons[-1].pressed.connect(_on_button_pressed.bind(optButtons[-1]));
 
 func _setup_focus():
 	#Hacemos una navegación circular (de arriba a abajo)
 	for i in buttons.size():
 		buttons[i].focus_neighbor_top = buttons[(i-1) % buttons.size()].get_path();
 		buttons[i].focus_neighbor_bottom = buttons[(i+1) % buttons.size()].get_path();
+		
+	#Hacemos también la navegación circular con los de las opciones
+	for i in optButtons.size():
+		optButtons[i].focus_neighbor_top = optButtons[(i-1) % optButtons.size()].get_path();
+		optButtons[i].focus_neighbor_bottom = optButtons[(i+1) % optButtons.size()].get_path();
+
+func _setup_options():
+	#Seteamos las resoluciones con la posible lista
+	var resolution = DisplayServer.screen_get_size();
+	optButtons[0].text = "RESOLUTION : %d x %d" % [resolution.x, resolution.y]; 
+	optButtons[1].text = "FULLSCREEN : " + _boolToString(false);
+	optButtons[2].text = "VSYNC : " + _boolToString(false);
 
 func _on_button_pressed(btn: TextureButton):
 	#Función encargada de recibir un botón y en base al botón seleccionado, llama una función
 	match btn.name:
 		"PlayBtn":
 			_playGame();
-		"HighscoreBtn":
-			_scorePressed();
 		"OptionsBtn":
 			_optionsPressed();
+		"BackBtn":
+			_backBtn();			
 
 func _input(event: InputEvent) -> void:
 	#Manejamos el sistema o confirmación de input
@@ -48,7 +78,73 @@ func _input(event: InputEvent) -> void:
 
 func _playGame():
 	SceneTransition._change_scene("game");
-func _scorePressed():
-	print("Highscore Pressed");
 func _optionsPressed():
-	print("Press Options");
+	buttons_section.visible = false;
+	options_section.visible = true;
+	
+	#Agarramos focus del botón de opciones
+	optButtons[0].grab_focus();
+func _backBtn():
+	#Guardamos las modificaciones visuales
+	
+	
+	#Desactivamos las UI necesarias
+	buttons_section.visible = true;
+	options_section.visible = false;
+	
+	#Agarramos focus del botón normal
+	buttons[0].grab_focus();
+
+#Funciones de la sección de opciones
+func _populateResolutions():
+	#Ésta función se encarga de asignar una lista posible de resoluciones de acuerdo al sistema
+	#operativo
+	
+	#Primero agarramos una lista de resoluciones
+	var resolutions = [
+		Vector2i(1920, 1080),
+		Vector2i(1600, 900),
+		Vector2i(1366, 768),
+		Vector2i(1280, 720),
+		Vector2i(1024, 576),
+		Vector2i(640, 360)
+	];
+	
+	#Limpiamos la lista de opciones y repopulamos
+	res_option.clear();
+	for res : Vector2i in resolutions:
+		res_option.add_item("%d x %d" % [res.x, res.y]);
+
+func _onResolutionSelected(index: int):
+	#Extraemos el texto de la resolución seleccionada
+	var selectedText = res_option.get_item_text(index);
+	#Con el texto obtenido, nos encargamos de extraer las resoluciones y separarlas del " x "
+	var parts = selectedText.split(" x ");
+	#Finalmente, con el array parts, conseguimos la variable de la resolución
+	var resolution = Vector2i(int(parts[0]), int(parts[1]));
+	
+	#Luego de ésto, asignamos el tamaño a la pantalla
+	get_window().size = resolution;
+
+func _onFullscreenToggled(toggle: bool):
+	if (toggle):
+		#Checamos que si está como verdadero, entonces asignamos el efecto de fullscreen
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN);
+		res_option.disabled = true;
+	else:
+		#Quitamos el fullscreen y reactivamos las resoluciones
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED);
+		#Reaplicamos la resolución de pasar de fullscreen
+		var selectedIndex = res_option.selected;
+		_onResolutionSelected(selectedIndex);
+		res_option.disabled = false;
+	
+func _onVSyncToggled(toggle: bool):
+	#Activamos y desactivamos el toggle
+	DisplayServer.window_set_vsync_mode(
+		DisplayServer.VSYNC_ENABLED if (toggle)
+		else DisplayServer.VSYNC_DISABLED
+	);
+
+func _boolToString(bool):
+	return "ON" if bool else "OFF";
